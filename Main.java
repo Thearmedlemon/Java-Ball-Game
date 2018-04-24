e68ad8a71da4f3f64a484c07b9e5960d4f47e73f
Joshua Murphy <joshmurf@live.co.uk>
2018-04-24 16:28:02 +0100

    Added labels to buttons using text class

--- Main.java
+++ Main.java
@@ -7,9 +7,12 @@ public class Main
 		boolean init = true;
 		GameArena board = new GameArena(x, y, init);
 		Button[] buttons = new Button[7];
+		Text[] buttonlabels = new Text[7];
 		for (x=1;x<6;x++){
 			 buttons[x] = new Button(150*x, 100, 100, 100, "WHITE",x);
+			 buttonlabels[x] = new Text(Integer.toString(x),(150*x)-15, 120, 50, "BLACK");
 		board.addRectangle(buttons[x]);
+		board.addText(buttonlabels[x]);
 	}
 	
 		while (true)
